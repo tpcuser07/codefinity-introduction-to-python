7dba03c8ccab09868cb536540d225184533cd4ff
tpcuser07 <>
2025-12-30 22:00:35 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,9 @@
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
